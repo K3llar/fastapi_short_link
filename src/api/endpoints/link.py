72ba9c945de780c6b33e71d54ebbe9318fc5c92d
Alexander Butakov <asbutakov@gmail.com>
2022-10-21 21:37:41 +0300

    add get method to link

--- src/api/endpoints/link.py
+++ src/api/endpoints/link.py
@@ -1,12 +1,14 @@
+from http import HTTPStatus
 from typing import List
 
 from fastapi import APIRouter, Depends
+from fastapi.responses import RedirectResponse
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.db import get_async_session
 from src.core.user import current_user, current_superuser, anonymous
-from src.crud.link import create_link
+from src.crud.link import create_link, get_full_link
 from src.schemas.link import LinkCreate, LinkDB
 from src.schemas.user import UserDB
 
@@ -28,3 +30,17 @@ async def create_new_link(
 ):
     new_link = await create_link(link, session, user)
     return new_link
+
+
+@router.get('/{short_link}',
+            response_class=RedirectResponse)
+async def get_link_by_short_link(
+        short_link: str,
+        session: AsyncSession = Depends(get_async_session),
+        user: UserDB = Depends(current_user)
+):
+    link = await get_full_link(short_link, session, user)
+    return RedirectResponse(
+        url=link.original_link,
+        status_code=HTTPStatus.TEMPORARY_REDIRECT
+    )
--- src/crud/link.py
+++ src/crud/link.py
@@ -20,7 +20,7 @@ async def create_link(
     new_link_data = new_link.dict()
     new_link_data['user_id'] = user.id
     if new_link_data.get('short_link') != '':
-        if await check_unique_short_link(
+        if await get_obj_by_short_link(
                 new_link_data['short_link'],
                 session
         ):
@@ -43,15 +43,40 @@ async def create_link(
     return db_link
 
 
-async def check_unique_short_link(
+async def get_obj_by_short_link(
         short_link: str,
         session: AsyncSession
 ):
     link = await session.execute(select(Link).where(
         Link.short_link == short_link
-    )
+        )
     )
     link = link.scalars().first()
     if link:
-        return True
-    return False
+        return link
+
+
+async def get_full_link(
+        short_link: str,
+        session: AsyncSession,
+        user: UserDB
+) -> Link:
+    full_link = await get_obj_by_short_link(short_link, session)
+    if not full_link:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=cst.NOT_FOUND.format(full_link)
+        )
+    if full_link.is_private is True:
+        if user.id != full_link.user_id:
+            raise HTTPException(
+                status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+                detail=cst.PRIVATE_URL
+            )
+    count = int(full_link.number_of_uses)
+    full_link.number_of_uses = count + 1
+    session.add(full_link)
+    await session.commit()
+    await session.refresh(full_link)
+    return full_link
+
